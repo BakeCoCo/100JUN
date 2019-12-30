e576ff9a7dc83b258e0cd320d7d90aa0754d45fb
BakeCoCo <58055835+BakeCoCo@users.noreply.github.com>
2019-12-30 21:42:13 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -791,3 +791,15 @@ public class Main{
 		}
     }
 }
+
+15596번 문제 정수 N개의 합
+
+public class Test {
+    long sum(int a[]) {
+        long ans = 0;
+        for(int i=0; i<a.length; i++) {
+        	ans += a[i];
+        }
+        return ans;
+    }
+}
