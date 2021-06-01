d338d7301b8ea7eecf75a9f96242d4461fb97c9e
Ha jun Lim <58055835+BakeCoCo@users.noreply.github.com>
2021-06-01 12:53:31 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1160,3 +1160,37 @@ public class Main {
         }
     }
 }
+
+1065번 문제 한수
+/*뭐 이딴 문제가 다있어*/
+
+import java.util.*;
+
+public class OneNumber01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num<100){
+            System.out.println(num);
+        }
+        else{
+            int result = 99;
+
+            for (int i = 111; i <= num; i++)
+                result += han(i);
+            System.out.println(result);
+        }
+    }
+
+    private static int han (int inum) {
+        int hun = inum / 100;
+        int ten = inum % 100 / 10;
+        int one = inum % 10;
+
+        if (ten*2 == hun + one) {
+            return 1;
+        }
+
+        return 0;
+    }
+}
